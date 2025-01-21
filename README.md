9e9fa614011f4d493b812d00efd2c2337a9f4af5
Mark Brown <mjbrown@microsoft.com>
2025-01-21 12:38:44 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# quickstart-nosql-rust
-Quickstart application template for Azure Cosmos DB for NoSQL and Rust
+<!--
+---
+page_type: sample
+name: "Quickstart: Azure Cosmos DB for NoSQL and Azure SDK for Rust"
+description: This is a simple web application to illustrate common basic usage of Azure Cosmos DB for NoSQL and the Azure SDK for Rust.
+urlFragment: template
+languages:
+- rust
+- azdeveloper
+products:
+- azure-cosmos-db
+---
+-->
+
+# Quickstart: Azure Cosmos DB for NoSQL - Azure SDK for Rust
+
+This is a simple web application to illustrate common basic usage of Azure Cosmos DB for NoSQL with the Azure SDK for Rust.
+
+## Prerequisites
+
+- [Docker](https://www.docker.com/)
+- [Azure Developer CLI](https://aka.ms/azd-install)
+- [Rust 1+](https://go.dev/dl/)
+
+## Quickstart
+
+1. Log in to Azure Developer CLI. *This is only required once per-install.*
+
+    ```bash
+    azd auth login
+    ```
+
+1. Initialize this template (`cosmos-db-nosql-rust-quickstart`) using `azd init`
+
+    ```bash
+    azd init --template cosmos-db-nosql-rust-quickstart
+    ```
+
+1. Ensure that **Docker** is running in your environment.
+
+1. Use `azd up` to provision your Azure infrastructure and deploy the web application to Azure.
+
+    ```bash
+    azd up
+    ```
+
+1. Observed the deployed web application
+
+    ![Screenshot of the deployed web application.](assets/web.png)
---
+++ src/app/main.rs
@@ -0,0 +1,61 @@
+use dotenv;
+use socketioxide::SocketIo;
+use socketioxide::extract::SocketRef;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+use tower_http::services::ServeDir;
+
+mod item;
+mod cosmos;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv().ok();
+
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/", |socket: SocketRef| {        
+        println!("Socket connected: {:?}", socket.id);
+        socket.on("start", move |socket: SocketRef| {
+            let handle_message = move |message: String| {                        
+                match socket.emit("new_message", &message) {
+                    Ok(_) => {
+                        println!("{}", message);
+                    }
+                    Err(e) => {
+                        println!("Failed to emit new message: {:?}", e);
+                    }
+                }
+            };
+
+            tokio::spawn(async move {
+                cosmos::run(
+                    std::env::var("CONFIGURATION__AZURECOSMOSDB__ENDPOINT").expect("Missing CONFIGURATION__AZURECOSMOSDB__ENDPOINT environment variable."),
+                    std::env::var("CONFIGURATION__AZURECOSMOSDB__DATABASENAME").expect("Missing CONFIGURATION__AZURECOSMOSDB__DATABASENAME environment variable."),
+                    std::env::var("CONFIGURATION__AZURECOSMOSDB__CONTAINERNAME").expect("Missing CONFIGURATION__AZURECOSMOSDB__CONTAINERNAME environment variable."),
+                    handle_message,
+                ).await;
+            });
+        });
+    });
+
+    let app = axum::Router::new()
+        .nest_service("/", ServeDir::new("static"))
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive())
+                .layer(layer),
+        );
+
+    let address = "0.0.0.0:3030".parse::<SocketAddr>().unwrap();
+
+    let listener = TcpListener::bind(address).await.unwrap();
+
+    println!("Listening on {}", address);
+
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
